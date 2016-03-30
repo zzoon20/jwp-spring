64fc3b9d5d6a76921b990cb21adfcbefca557f3c
javajigi <javajigi@gmail.com>
2016-03-30 16:46:12 +0900

    GET method를 사용하도록 수정함.

--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -2,18 +2,19 @@ package next.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import next.dao.QuestionDao;
 
 @Controller
 public class HomeController {
-    private QuestionDao questionDao = QuestionDao.getInstance();
+	private QuestionDao questionDao = QuestionDao.getInstance();
 
-    @RequestMapping("/")
-    public ModelAndView home() throws Exception {
-    	ModelAndView mav = new ModelAndView("index");
-    	mav.addObject("questions", questionDao.findAll());
-        return mav;
-    }
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView home() throws Exception {
+		ModelAndView mav = new ModelAndView("index");
+		mav.addObject("questions", questionDao.findAll());
+		return mav;
+	}
 }

f5aa115f91c6816acf345822ebbaab1c752f4ad4
zzoon20 <zzoon20@10.211.55.2>
2016-07-29 16:54:52 +0900

    UserController /users/login 구현

--- src/main/java/next/controller/user/UserController.java
+++ src/main/java/next/controller/user/UserController.java
@@ -7,30 +7,48 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import next.controller.UserSessionUtils;
 import next.dao.UserDao;
+import next.model.User;
 
 @Controller
 public class UserController {
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
-    private UserDao userDao = UserDao.getInstance();
-    
-    @RequestMapping(value="/users", method=RequestMethod.GET)
-    public ModelAndView listUser(HttpSession session) throws Exception {
-    	if (!UserSessionUtils.isLogined(session)) {
+	private UserDao userDao = UserDao.getInstance();
+
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	public ModelAndView listUser(HttpSession session) throws Exception {
+		if (!UserSessionUtils.isLogined(session)) {
 			return new ModelAndView("redirect:users/loginForm");
 		}
-    	
-        ModelAndView mav = new ModelAndView("user/list");
-        mav.addObject("users", userDao.findAll());
-        return mav;
-    }
 
-    @RequestMapping(value="/users/loginForm", method=RequestMethod.GET)
-    public ModelAndView loginForm() throws Exception {
-    	return new ModelAndView("user/login");
-    }
-    
+		ModelAndView mav = new ModelAndView("user/list");
+		mav.addObject("users", userDao.findAll());
+		return mav;
+	}
+
+	@RequestMapping(value = "/users/loginForm", method = RequestMethod.GET)
+	public ModelAndView loginForm() throws Exception {
+		return new ModelAndView("user/login");
+	}
+
+	@RequestMapping(value = "/users/login", method = RequestMethod.POST)
+	public ModelAndView login(HttpSession session, @RequestParam String userId, @RequestParam String password) throws Exception {
+        User user = userDao.findByUserId(userId);
+        
+        if (user == null) {
+            throw new NullPointerException("사용자를 찾을 수 없습니다.");
+        }
+        
+        if (user.matchPassword(password)) {
+            session.setAttribute("user", user);
+            return new ModelAndView("redirect:/");
+        } else {
+            throw new IllegalStateException("비밀번호가 틀립니다.");
+        }
+	}
+
 }

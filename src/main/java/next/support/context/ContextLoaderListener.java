3e445b79e2bbe2617ab4816cb8900a23283e6cfc
youngjun <youngjun@192.168.0.21>
2016-08-04 22:08:42 +0900

    Spring에서 DB초기화

--- src/main/java/next/support/context/ContextLoaderListener.java
+++ src/main/java/next/support/context/ContextLoaderListener.java
@@ -1,31 +1,28 @@
 package next.support.context;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.stereotype.Component;
 
-import core.jdbc.ConnectionManager;
-
-@WebListener
-public class ContextLoaderListener implements ServletContextListener {
+@Component
+public class ContextLoaderListener implements InitializingBean {
 	private static final Logger logger = LoggerFactory.getLogger(ContextLoaderListener.class);
+	@Autowired
+	private DataSource dataSource;
 	
 	@Override
-	public void contextInitialized(ServletContextEvent sce) {
+	public void afterPropertiesSet() throws Exception {
 		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
 		populator.addScript(new ClassPathResource("jwp.sql"));
-		DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+		DatabasePopulatorUtils.execute(populator, dataSource);
 		
-		logger.info("Completed Load ServletContext!");
-	}
-
-	@Override
-	public void contextDestroyed(ServletContextEvent sce) {
+		logger.info("Completed Load ServletContext!");		
 	}
 }

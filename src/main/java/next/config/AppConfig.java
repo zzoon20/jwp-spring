6be941dc88975dc217b3c57b94ab7f7554348f62
youngjun <youngjun@192.168.0.21>
2016-08-11 19:42:20 +0900

    env 활용

--- src/main/java/next/config/AppConfig.java
+++ src/main/java/next/config/AppConfig.java
@@ -3,10 +3,13 @@ package next.config;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 
@@ -15,19 +18,18 @@ import org.springframework.stereotype.Controller;
 	basePackages = { "next.service", "next.dao" },
 	excludeFilters = @ComponentScan.Filter(value = Controller.class, type = FilterType.ANNOTATION)
 )
+@PropertySource("classpath:application.properties")
 public class AppConfig {
-	private static final String DB_DRIVER = "org.h2.Driver";
-	private static final String DB_URL = "jdbc:h2:~/jwp-basic;AUTO_SERVER=TRUE";
-	private static final String DB_USERNAME = "sa";
-	private static final String DB_PW = "";
+	@Autowired
+	private Environment env;
 	
 	@Bean
 	public DataSource dataSource() {
 		BasicDataSource ds = new BasicDataSource();
-		ds.setDriverClassName(DB_DRIVER);
-		ds.setUrl(DB_URL);
-		ds.setUsername(DB_USERNAME);
-		ds.setPassword(DB_PW);
+		ds.setDriverClassName(env.getProperty("db.driver"));
+		ds.setUrl(env.getProperty("db.url"));
+		ds.setUsername(env.getProperty("db.username"));
+		ds.setPassword(env.getProperty("db.password"));
 		return ds;
 	}
 	
